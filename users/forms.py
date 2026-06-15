7dc4e934857e7e60591bf11dedc268d8fb19e393
MohammadHasan Anisi <mhanisi568@gmail.com>
2026-06-16 00:43:15 +0330

    Create a def clean_phone_number

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,15 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('phone_number')
 
+    def clean_phone_number(self):
+
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if not phone_number.isdigit():
+            raise forms.ValidationError("Phone number must be number")
+
+        if len(phone_number) < 11 or len(phone_number) > 13:
+            raise forms.ValidationError("Phone number length is not elven")
+
+        return phone_number
+
