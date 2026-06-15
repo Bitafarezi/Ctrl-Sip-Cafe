6678a6b140a02a8cdd45a5a7ba793518a9c64a42
Bita <bitafarezi79@gmail.com>
2026-06-15 14:54:25 +0330

    Complete models for the users app

--- users/models.py
+++ users/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 # Create your models here.
 
+
+# This model won't create any table in database, it is used only for inheritance
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created_date"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Updated_date"
+    )
+
+    class Meta:
+        abstract = True
+
+
 class CustomUserManager(BaseUserManager):
     
     def create_user(self, phone_number, password=None, **extra_fields):
@@ -29,13 +43,25 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(phone_number, password, **extra_fields)
 
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser, BaseModel):
     username = None
-    phone_number = models.CharField(max_length=13, unique=True, verbose_name='Phone Number')
+    phone_number = models.CharField(max_length=13, unique=True, verbose_name='Phone_number')
+    favorites = models.ManyToManyField("products.Product", blank=True, related_name="favorited_by", verbose_name="Favorite_products")
     
     # We specified that the login field for the user is phone number
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ([])
+    REQUIRED_FIELDS = []
     
     def __str__(self):
         return self.phone_number
+    
+
+class UserProfile(BaseModel):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="First Name")
+    last_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Last Name")
+    display_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Display Name / Username")
+    image = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name="Profile Picture")
+
+    def __str__(self):
+        return f"Profile for {self.user.phone_number}"
