0825de1982bd30e96af2d3b36ef8c4f19ce23207
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-06-16 01:18:19 +0330

    Add User_login_view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, View
 
 from .forms import CustomUserCreationForm
 
@@ -9,10 +12,16 @@ class RegisterView(CreateView):
     template_name = "users/register.html" 
     success_url = reverse_lazy(
         "login"
-    )
+    ) 
 
     def form_valid(self, form):
         response = super().form_valid(form)
         user = form.instance
         return response
 
+
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("menu") 
