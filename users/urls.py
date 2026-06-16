c95cd50a3e36b14f513ccbe9db2388370f9918ac
Bita <bitafarezi79@gmail.com>
2026-06-16 14:08:07 +0330

    Add staticfiles_finder in the main settings

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_callable(), name='register'),
-    path('login/', UserLoginView.as_callable(), name='login'),
-    path('logout/', UserLogoutView.as_callable(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
