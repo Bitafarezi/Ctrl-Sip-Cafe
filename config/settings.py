af8d234b728dd96bcd4db79a8152dba4f23bd163
Bita <bitafarezi79@gmail.com>
2026-06-20 23:22:06 +0330

    Add orders app

--- config/settings.py
+++ config/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+    'orders.app.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
-    'shop.apps.ShopConfig',
-    'cart.apps.CartConfig',   
 ]
 
 MIDDLEWARE = [
