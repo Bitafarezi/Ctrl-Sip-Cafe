f8a79fe2cb44cb9d1cf1fa9d430b0c19f146aa63
Bita <bitafarezi79@gmail.com>
2026-06-24 14:50:05 +0330

    Debug views for orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
 
-app_name = 'orders'
+from orders import views
+
+app_name = "orders"
 
 urlpatterns = [
-    path('checkout/', views.checkout, name='checkout'),
-    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('create', views.create_order, name='create_order'),
+    path('list', views.user_orders, name='user_orders'),
+    path('checkout/<int:order_id>', views.checkout, name='checkout'),
+    path('fake-payment/<int:order_id>', views.fake_payment, name='pay_order')
 ]
