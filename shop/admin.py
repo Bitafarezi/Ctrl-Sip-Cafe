59e634bca05726014833256e8d78d17b88c3dbb9
Bita <bitafarezi79@gmail.com>
2026-06-24 12:32:02 +0330

    Show images in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, ProductImage
 
 # Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3  
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    prepopulated_fields = {'slug': ('title',)}
