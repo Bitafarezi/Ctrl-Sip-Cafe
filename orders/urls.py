f872d08e232f5d67076baa5195fe52fea8c2f071
Bita <bitafarezi79@gmail.com>
2026-06-24 14:38:55 +0330

    Add urls for order app

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
+from . import views
 
-from orders import views
-
-app_name = "orders"
+app_name = 'orders'
 
 urlpatterns = [
-    path('create', views.create_order, name='create_order'),
-    path('list', views.user_orders, name='user_orders'),
-    path('checkout/<int:order_id>', views.checkout, name='checkout'),
-    path('fake-payment/<int:order_id>', views.fake_payment, name='pay_order')
+    path('checkout/', views.checkout, name='checkout'),
+    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
 ]
