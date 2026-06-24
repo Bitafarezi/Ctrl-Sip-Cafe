f0baa431f38498cce969248bd2940bc1c456dce5
Bita <bitafarezi79@gmail.com>
2026-06-24 14:55:04 +0330

    Add Notification model for sending order notifaction

--- orders/views.py
+++ orders/views.py
@@ -16,6 +16,11 @@ def create_order(request):
         OrderItem.objects.create(
             order=order, product=item['product'],
             price=item['price'], quantity=item['quantity']
+        )
+    # Send notification to admin
+    from shop.models import Notification
+    Notification.objects.create(
+        message=f"🛒 New Order from {request.user.full_name} - Price: ${order.get_total_price}"
     )
     return redirect('orders:pay_order', order_id=order.id)
 
