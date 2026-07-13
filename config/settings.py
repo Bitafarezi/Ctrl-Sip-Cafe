b8190797c6465329c199bcac87caba5deae0f190
Bita <bitafarezi79@gmail.com>
2026-07-13 12:25:53 +0330

    Configure CorsMiddleware

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',        # must be on the top of all
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True                       # to coonect the site easily through localhost by API
 
 TEMPLATES = [
     {
