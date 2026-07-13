1694964c9f80f91100202c904cc29f4b51798b94
Bita <bitafarezi79@gmail.com>
2026-07-13 13:23:52 +0330

    Debug

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product, Category, ProductImage, Comment
+from .models import Product, Category, ProductImage
 
 
 class CategorySerializer(serializers.ModelSerializer):
